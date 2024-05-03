c807af969850f50e880d3eb4578c4412f06112d1
Antonino Balistreri <161389988+ninobal04@users.noreply.github.com>
2024-05-02 18:51:54 -0700

    Create init.h

---
+++ Assignments/Assignment_SafeBox/init.h
@@ -0,0 +1,15 @@
+
+//* Program Function Declaration *//
+void restart();
+void readphoto();
+void emergency();
+void display1();
+void display2();
+void compare();
+void buzzer();
+
+
+//* Variables *//
+int  pR1        = 0;
+int  pR2        = 0;
+int  secretcode = 0x33;
