9ef6f4c8d47fac264168d20cc8423c2edf95b5f6
Antonino Balistreri <161389988+ninobal04@users.noreply.github.com>
2024-05-11 17:56:20 -0700

    Create final_PWM.c

---
+++ Projects/EE310 Final Project/final_PWM.c
@@ -0,0 +1,20 @@
+//---------------------
+// Title: EE310 Final Project
+//---------------------
+// Program Details: Using PWM to manipulate an RGB LED.
+// 	
+// Purpose: To add a peripheral to our final project
+//
+// File Dependencies / Libraries: None 
+// Compiler: XC8, v2.46
+// Platform: MPLABX, v6.20
+// Operating System: MACOSX, Windows 11
+// Author(s): Antonino Balistreri, Cameron deLeeuw
+// Outputs:  
+// Inputs: 
+// Date: MAY 11, 2024
+// Versions:
+//       V1.0: 5/11/2024 - First Version - Project Creation
+
+#include <xc.h>
+#include <finalHeader.h>
